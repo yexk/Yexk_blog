09200d06ab332af5419133601ebc8ff6cc26d9c8
Yexk <yexk@yexk.com>
2016-11-21 16:07:16 +0800

    去掉已没用的文件

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
@@ -12,6 +12,8 @@
 // [ 应用入口文件 ]
 
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
+define('APP_PATH', __DIR__ . '/../yexk/');
+// 定义config目录
+define('CONF_PATH', __DIR__ . '/../config/');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
